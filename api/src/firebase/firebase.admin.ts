afdbf8d15c4d6f5c7b7eed8da302d84ce19be83b
Milan Radeta <milan.radeta@itengine.rs>
2022-04-22 17:55:51 +0200

    Fix firebase config

--- api/src/firebase/firebase.admin.ts
+++ api/src/firebase/firebase.admin.ts
@@ -3,5 +3,5 @@ import admin from "firebase-admin"
 import { FIREBASE_ADMIN_CONFIG } from "./firebase.config";
 
 admin.initializeApp({
-  credential: admin.credential.cert(FIREBASE_ADMIN_CONFIG)
+  credential: admin.credential.cert(JSON.stringify(FIREBASE_ADMIN_CONFIG))
 });
--- api/src/firebase/firebase.client.ts
+++ api/src/firebase/firebase.client.ts
@@ -1,10 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 import { FIREBASE_WEB_APP_CONFIG } from "./firebase.config";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Initialize Firebase
-const app = initializeApp(FIREBASE_WEB_APP_CONFIG);
-const analytics = getAnalytics(app);
+export const FIREBASE_APP = initializeApp(FIREBASE_WEB_APP_CONFIG);
+// const analytics = getAnalytics(app);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+FIREBASE_AUTH.setPersistence({ type: "NONE" });
