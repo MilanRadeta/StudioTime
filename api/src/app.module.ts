099e07beb64a06520f17c2a18f2b777a652f9e78
Milan Radeta <milan.radeta@itengine.rs>
2022-04-23 17:48:30 +0200

    Remove auth controller

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/auth.module';
 import { RehearsalModule } from './modules/rehearsal/rehearsal.module';
 import { FirebaseAuthGuard } from './modules/shared/guards/firebase-auth.guard';
 import { FirebaseAuthStrategy } from './modules/shared/strategies/firebase-auth.strategy';
@@ -12,7 +11,6 @@ import { UserModule } from './modules/user/user.module';
 @Module({
   imports: [
     UserModule,
-    AuthModule,
     StudioModule,
     RehearsalModule
   ],
