757677e43e1e7007918106bd2c50f06dd888e8cb
Milan Radeta <milan.radeta@itengine.rs>
2022-04-25 20:04:25 +0200

    Store token to local storage

--- web/src/auth/AuthContext.tsx
+++ web/src/auth/AuthContext.tsx
@@ -2,20 +2,21 @@ import React, { useCallback } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { MAIN_ROUTES } from "../Main";
 
+const key = 'TOKEN';
 
 interface AuthContextType {
   token: string;
   setToken: (value: string) => void
 }
 
-
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [token, setToken] = React.useState<any>(null);
+  const [token, setToken] = React.useState<string>(localStorage.getItem(key) ?? '');
   const navigate = useNavigate();
   const location = useLocation();
   const state = location.state as { from: Location };
 
   const saveToken = useCallback((token: string) => {
+    localStorage.setItem(key, token);
     setToken(token);
     navigate(token ? (state?.from?.pathname || '') : MAIN_ROUTES.AUTH);
   }, [state, navigate, setToken]);
